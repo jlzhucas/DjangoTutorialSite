cba0603b8c5e3f69b65ccd4a2617162624de55ed
Jinliang Zhu <zhujinlianghust@gmail.com>
2013-03-24 19:44:53 +0800

    Use class-based view instead of funciton-based view

--- DjangoTutorialSite/mysite/mysite/apps/polls/urls.py
+++ DjangoTutorialSite/mysite/mysite/apps/polls/urls.py
@@ -1,12 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
+from mysite.apps.polls.views import IndexView
+from mysite.apps.polls.views import DetailView
+from mysite.apps.polls.views import ResultsView
+from mysite.apps.polls.views import VoteView
 
 
 urlpatterns = patterns('mysite.apps.polls.views',
-    url(r'^$', 'index', name="index"),
-    url(r'^(?P<poll_id>\d+)/$', 'detail', name="detail"),
-    url(r'^(?P<poll_id>\d+)/results/$', 'results', name="results"),
-    url(r'^(?P<poll_id>\d+)/vote/$', 'vote', name="vote"),
+    url(r'^$', IndexView.as_view(), name="index"),
+    url(r'^(?P<poll_id>\d+)/$', DetailView.as_view(), name="detail"),
+    url(r'^(?P<poll_id>\d+)/results/$', ResultsView.as_view(), name="results"),
+    url(r'^(?P<poll_id>\d+)/vote/$', VoteView.as_view(), name="vote"),
 
 )
 
--- DjangoTutorialSite/mysite/mysite/apps/polls/views.py
+++ DjangoTutorialSite/mysite/mysite/apps/polls/views.py
@@ -1,46 +1,76 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.views.generic import TemplateView
 
 from mysite.apps.polls.models import Poll, Choice
 
 
-def index(request):
+class IndexView(TemplateView):
+    ''' Index page view. '''
+
     template_name = "polls/index.html"
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')
-    output = ', '.join([p.question for p in latest_poll_list])
-    context = {'latest_poll_list': latest_poll_list}
-    return render_to_response(template_name, context)
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        latest_poll_list = Poll.objects.all().order_by('-pub_date')
+        context.update({'latest_poll_list': latest_poll_list})
+        return context
+        
 
-def detail(request, poll_id):
+class DetailView(TemplateView):
+    ''' Poll detail page view. '''
+
     template_name = "polls/detail.html"
-#     try:
-#         p = Poll.objects.get(pk=poll_id)
-#     except Poll.DoesNotExist:
-#         raise Http404
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response(template_name, {'poll': p})
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailView, self).get_context_data(**kwargs)
+        poll_id = kwargs.get("poll_id", None) 
+        p = get_object_or_404(Poll, pk=poll_id)
+        context.update({"poll": p})
+        return context
 
 
-def results(request, poll_id):
+class ResultsView(TemplateView):
+    ''' Poll results page view. '''
+
     template_name = "polls/results.html"
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response(template_name, {'poll': p})
+
+    def get_context_data(self, **kwargs):
+        context = super(ResultsView, self).get_context_data(**kwargs)
+        poll_id = kwargs.get("poll_id", None)
+        p = get_object_or_404(Poll, pk=poll_id)
+        context.update({"poll": p})
+        return context
 
 
-def vote(request, poll_id):
+class VoteView(TemplateView):
+    ''' Poll vote page view. '''
+    
     template_name = "polls/detail.html"
-    p = get_object_or_404(Poll, pk=poll_id)
-    try:
-        selected_choice = p.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render_to_response(template_name, {
-            'poll': p,
-            'error_message': "You did not select a choice."
-        }, context_instance=RequestContext(request))
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('detail', args=(p.id, )))
+
+    def get(self, request, *args, **kwargs):
+        poll_id = kwargs.get("poll_id", None)
+        p = get_object_or_404(Poll, pk=poll_id)
+        context = self.get_context_data(**kwargs)
+        context.update({"poll": p})
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        poll_id = kwargs.get("poll_id", None)
+        p = get_object_or_404(Poll, pk=poll_id)
+        context = self.get_context_data(**kwargs)
+        try:
+            selected_choice = p.choice_set.get(pk=request.POST['choice'])
+        except (KeyError, Choice.DoesNotExist):
+            context.update({
+                "poll": p,
+                "error_message": "You did not select a choice."
+            })
+            return self.render_to_response(context)
+        else:
+            selected_choice.votes += 1
+            selected_choice.save()
+            return HttpResponseRedirect(reverse('detail', args=(p.id, )))
+
